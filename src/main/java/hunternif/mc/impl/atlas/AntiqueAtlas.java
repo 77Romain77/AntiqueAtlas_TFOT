5986111e0b1573b7b78fb2fe9afa735ccf60fc4e
Stereowalker <stereowalker.mc@gmail.com>
2025-07-30 03:37:35 +0100

    Sorted some mixins

--- src/main/java/hunternif/mc/impl/atlas/AntiqueAtlas.java
+++ src/main/java/hunternif/mc/impl/atlas/AntiqueAtlas.java
@@ -158,9 +158,11 @@ public class AntiqueAtlas extends MinecraftMod implements PacketHolder {
     
     @Override
     public void setupRegistries(RegistryCollector collector) {
-    	collector.addRegistryHolder(Registries.ITEM, AntiqueAtlasItems.Items.class);
-    	collector.addRegistryHolder(Registries.RECIPE_SERIALIZER, AntiqueAtlasItems.Recipes.class);
-    	collector.addRegistryHolder(Registries.DATA_COMPONENT_TYPE, AntiqueAtlasItems.Components.class);
+    	if (AntiqueAtlas.CONFIG.itemNeeded) {    		
+    		collector.addRegistryHolder(Registries.ITEM, AntiqueAtlasItems.Items.class);
+    		collector.addRegistryHolder(Registries.RECIPE_SERIALIZER, AntiqueAtlasItems.Recipes.class);
+    		collector.addRegistryHolder(Registries.DATA_COMPONENT_TYPE, AntiqueAtlasItems.Components.class);
+    	}
     }
 
 	@Override
